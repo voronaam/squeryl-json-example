2ff14b16a21215da2cb5b3e1483859a16b52c841
Aleksey Vorona <aleksey@genomedx.com>
2014-03-11 13:07:21 -0700

    Difference in Query.statement and the real query

--- src/main/scala/example/JsonCustomTypes.scala
+++ src/main/scala/example/JsonCustomTypes.scala
@@ -7,28 +7,34 @@ import java.sql.ResultSet
 import org.squeryl.adapters.H2Adapter
 import java.util.Date
 import scala.collection.mutable.HashMap
+import org.squeryl.annotations.Column
+import org.squeryl.dsl.ast.FunctionNode
+import org.squeryl.dsl.ast.LogicalBoolean
 
 object XsnTypeMode extends PrimitiveTypeMode {
   import com.codahale.jerkson.Json._
   import scala.language.implicitConversions
-  
+
   implicit val jsonTEF = new NonPrimitiveJdbcMapper[String, HashMap[String, Any], TString](stringTEF, this) {
-    def convertFromJdbc(v: String) = if(v == null) null else parse[HashMap[String, Any]](v)
-    def convertToJdbc(v: HashMap[String, Any]) = if(v == null) null else generate(v)
+    def convertFromJdbc(v: String) = if (v == null) null else parse[HashMap[String, Any]](v)
+    def convertToJdbc(v: HashMap[String, Any]) = if (v == null) null else generate(v)
     override def sample = new HashMap[String, Any]()
   }
-  
-  implicit def mapToTE(s: HashMap[String, Any]) = jsonTEF.create(s)  
-  
+
+  implicit def mapToTE(s: HashMap[String, Any]) = jsonTEF.create(s)
+
   // Support for Option[Map]
   implicit val optionJsonTEF =
-    new TypedExpressionFactory[Option[HashMap[String, Any]], TOptionString]
-      with DeOptionizer[String, HashMap[String, Any], TString, Option[HashMap[String, Any]], TOptionString] {
+    new TypedExpressionFactory[Option[HashMap[String, Any]], TOptionString] with DeOptionizer[String, HashMap[String, Any], TString, Option[HashMap[String, Any]], TOptionString] {
 
-    val deOptionizer = jsonTEF
-  }
-  
+      val deOptionizer = jsonTEF
+    }
+
   implicit def optionMapToTE(s: Option[HashMap[String, Any]]) = optionJsonTEF.create(s)
+  
+  def custom[A1,T1](s: TypedExpression[A1,T1]) =
+    new FunctionNode("custom", Seq(s)) with LogicalBoolean
+
 }
 
 
@@ -49,44 +55,94 @@ object JsonTesterSchema extends Schema {
 object OptionJsonTesterSchema extends Schema {
   val optionTimestampTester = table[OptionJsonTester]
 }
-
+class Article( @Column("id") val id: Int, @Column("title") val title: String)
+object ArticlesSchema extends Schema {
+  val articles = table[Article]
+}
 
 object JsonTests {
-  
+
   def main(args: Array[String]): Unit = {
     println("Starting a JSON test")
     Class.forName("org.h2.Driver")
-    SessionFactory.concreteFactory = Some(() =>
-      Session.create(
+    SessionFactory.concreteFactory = Some(() => {
+      val session = Session.create(
         java.sql.DriverManager.getConnection("jdbc:h2:mem:test", "sa", ""),
-        new H2Adapter))
-        
-    test // works
-    testOption // fails
+        new H2Adapter)
+      session.setLogger((sql: String) => println(s"SQL: $sql"))
+      session
+        })
+
+//    test // works
+//    testOption // fails
+//    testRs
+      testQ
   }
 
-  def testMap = new HashMap[String, Any]{put("key","value")}
+  def testMap = new HashMap[String, Any] { put("key", "value") }
 
   def test = transaction {
     try { JsonTesterSchema.drop } catch { case e: Exception => {} }
     JsonTesterSchema.create
 
     JsonTesterSchema.timestampTester.insert(new JsonTester(testMap))
-    
-    val value = inTransaction{ from(JsonTesterSchema.timestampTester)( s => select(s)).single }
+
+    val value = inTransaction {
+      from(JsonTesterSchema.timestampTester)(s => select(s)).single
+    }
 
     println(value)
   }
-  
+
   def testOption = transaction {
     try { OptionJsonTesterSchema.drop } catch { case e: Exception => {} }
     OptionJsonTesterSchema.create
 
     OptionJsonTesterSchema.optionTimestampTester.insert(new OptionJsonTester(Some(testMap)))
-    
-    val value = inTransaction{ from(OptionJsonTesterSchema.optionTimestampTester)( s => select(s)).single }
+
+    val value = inTransaction { from(OptionJsonTesterSchema.optionTimestampTester)(s => select(s)).single }
 
     println(value)
+  }
+
+  def testRs = {
+    println(JsonTesterSchema.timestampTester.posoMetaData)
+    val holder = java.sql.DriverManager.getConnection("jdbc:h2:mem:test", "sa", "")
+    transaction {
+      try { JsonTesterSchema.drop } catch { case e: Exception => {} }
+      JsonTesterSchema.create
+      JsonTesterSchema.timestampTester.insert(new JsonTester(testMap))
+    }
+    inTransaction {
+      val q = from(ArticlesSchema.articles)(s => where(custom(upper(s.title))) select(s))
+      println(q.statement)
+    }
+    val c = java.sql.DriverManager.getConnection("jdbc:h2:mem:test", "sa", "")
+    val rs = c.prepareStatement(s"select * from ${JsonTesterSchema.timestampTester.name}").executeQuery()
+    rs.next()
+    val result = new JsonTester
+    JsonTesterSchema.timestampTester.posoMetaData.findFieldMetaDataForProperty("extra").map(md => md.setFromResultSet(result, rs, 1))
+    println(result)
+//    while (rs.next()) {
+//      println(rs.getString(1))
+//    }
+    c.close()
+    holder.close
+  }
+  
+  def testQ = {
+    val holder = java.sql.DriverManager.getConnection("jdbc:h2:mem:test", "sa", "")
+    transaction {
+      ArticlesSchema.create
+      val products: List[Option[String]] = List(Some("content"), Some("2"), Some("3"))
+      val (n1 :: n2 :: n3 :: rest) = products
+      val ls = from(ArticlesSchema.articles)(a => where(a.title like n1.?) select(a.id))
+      println("Testing the Query.statement method")
+      println(ls.statement)
+      println
+      println("Testing the real query")
+      ls.toList
+    }
   }
 
 }
